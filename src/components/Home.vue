1fff2f2111cc1951905a3fa746ab0e404d462e6e
afrasiabi1374 <mohammad.aligiga@gmail.com>
2021-09-29 19:22:24 +0330

    store getter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,53 @@
 <template>
-  <h4>Home component</h4>
-  <h5>{{ count }}</h5>
-  <h5>{{name}}</h5>
+  <h1>todos</h1>
+  <h5 v-for="todo in todos" :key="todo.id">
+      {{todo.text}}
+  </h5>
+  <hr>
+    <h2>done todos</h2>
+  <h5 v-for="todo in doneTodos" :key="todo.id">
+      {{todo.text}}
+  </h5>
+
 </template>
 
 <script>
-import {mapState} from 'vuex'
+// import {mapState} from 'vuex'
 // import { computed } from '@vue/reactivity'
 // import {useStore} from 'vuex'
 export default {
-    // computed:{
-    //     count(){
-    //         return this.$store.state.count
-    //     }
-    // }
-
+    //ravesh mamooli
     computed:{
-        // ...mapState(['count']),
-
-        // ...mapState({
-        //     count:'count'
-        // }),
-
-        // ...mapState(['count']),
-        ...mapState({
-            count : (state) => state.count
-        }),
-        name(){
-            return 'ali'
+        todos(){
+            return this.$store.state.todos.filter(todo =>{
+                    return todo
+            })
+        },
+        doneTodos(){
+            return this.$store.getters.doneTodos
         }
     }
 
+    // computed:{
+    //     //ravesh1
+    //     // ...mapState(['count']),
+    //     //ravesh2
+    //     // ...mapState({
+    //     //     count:'count'
+    //     // }),
+    //     //ravesh3
+    //     // ...mapState(['count']),
+    //     //ravesh4
+    //     ...mapState({
+    //         count : (state) => state.count
+    //     }),
+    //     name(){
+    //         return 'ali'
+    //     }
+    // }
+
+
+    //raveshe composition
     // setup(){
     //     const store = useStore()
     //     const count = computed(()=>store.state.count)
@@ -39,6 +56,8 @@ export default {
     //         count
     //     }
     // }
+
+
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
 import { createStore } from 'vuex'
 const store = createStore({
-    state(){
-        return {
-            count : 110
+    state:{
+        
+        todos:[
+            {id:1, text : 'todo 1', done:true},
+            {id:2, text : 'todo 2', done:false},
+        ]
+        
+    },
+    getters: {
+        doneTodos(state){
+            console.log(state);
+            return state.todos.filter(todo=>todo.done===true)
         }
     }
 })

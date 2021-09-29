08dc2224fd95879b358aef509fec43068d7ad42a
afrasiabi1374 <mohammad.aligiga@gmail.com>
2021-09-29 16:35:26 +0330

    store state

---
+++ src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <h4>Home component</h4>
+  <h5>{{ count }}</h5>
+  <h5>{{name}}</h5>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+// import { computed } from '@vue/reactivity'
+// import {useStore} from 'vuex'
+export default {
+    // computed:{
+    //     count(){
+    //         return this.$store.state.count
+    //     }
+    // }
+
+    computed:{
+        // ...mapState(['count']),
+
+        // ...mapState({
+        //     count:'count'
+        // }),
+
+        // ...mapState(['count']),
+        ...mapState({
+            count : (state) => state.count
+        }),
+        name(){
+            return 'ali'
+        }
+    }
+
+    // setup(){
+    //     const store = useStore()
+    //     const count = computed(()=>store.state.count)
+    //     return{
+    //         store,
+    //         count
+    //     }
+    // }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex'
+const store = createStore({
+    state(){
+        return {
+            count : 110
+        }
+    }
+})
+
+export default store

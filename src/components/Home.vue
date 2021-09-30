6e2658a0dc5cfd0dedf3558194baf7fb50095a08
afrasiabi1374 <mohammad.aligiga@gmail.com>
2021-09-30 16:44:38 +0330

    mutations

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,32 +1,30 @@
 <template>
-  <h1>todos</h1>
-  <h5 v-for="todo in todos" :key="todo.id">
-      {{todo.text}}
-  </h5>
-  <hr>
-    <h2>done todos</h2>
-  <h5 v-for="todo in doneTodos" :key="todo.id">
-      {{todo.text}}
-  </h5>
-
+    <div>
+        <h2>
+            home component
+        </h2>
+        <h4>
+            {{count}}
+        </h4>
+        <button @click="increment">increment</button>
+    </div>
 </template>
 
 <script>
 // import {mapState} from 'vuex'
 // import { computed } from '@vue/reactivity'
-// import {useStore} from 'vuex'
+import {useStore} from 'vuex'
+import {computed} from 'vue'
 export default {
     //ravesh mamooli
     computed:{
         todos(){
             return this.$store.state.todos.filter(todo =>{
-                    return todo
+                    return todo.text
             })
         },
-        doneTodos(){
-            return this.$store.getters.doneTodos
-        }
-    }
+
+    },
 
     // computed:{
     //     //ravesh1
@@ -48,14 +46,23 @@ export default {
 
 
     //raveshe composition
-    // setup(){
-    //     const store = useStore()
-    //     const count = computed(()=>store.state.count)
-    //     return{
-    //         store,
-    //         count
-    //     }
-    // }
+    setup(){
+        const store = useStore();
+        const doneTodos = computed(() => store.getters.doneTodos)
+        const getTodoById = computed(() => store.getters.getTodoById(2))
+        const count = computed(()=>store.state.count)
+        function increment(){
+            store.commit('INCREMENT',2)
+        }
+
+
+        return{
+            doneTodos,
+            getTodoById,
+            count,
+            increment
+        }
+    }
 
 
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from 'vuex'
+import {INCREMENT} from "./mutationType";
 const store = createStore({
     state:{
-        
+        count:1,
         todos:[
             {id:1, text : 'todo 1', done:true},
             {id:2, text : 'todo 2', done:false},
@@ -12,6 +13,15 @@ const store = createStore({
         doneTodos(state){
             console.log(state);
             return state.todos.filter(todo=>todo.done===true)
+        },
+        getTodoById : (state) => (id) => {
+            console.log('filter todo',state.todos.find(todo => todo.id === id));
+            return state.todos.filter(todo => todo.id == id)
+        }
+    },
+    mutations: {
+        [INCREMENT](state,number){
+            state.count+=number
         }
     }
 })
